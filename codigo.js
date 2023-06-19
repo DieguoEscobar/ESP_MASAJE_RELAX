509bdc59d95756a0f4a9323b7d3f1383a12f4add
Dieguo <dieguoescobar@gmail.com>
2023-06-19 14:04:21 -0500

    casi completo

---
+++ codigo.js
@@ -0,0 +1,35 @@
+function mostrar(){
+    document.getElementById("masajes-reductores").style.display="block";
+}
+
+function ocultar(){
+    document.getElementById("masajes-reductores").style.display="none";
+    document.getElementById("masajes-relajantes").style.display="none";
+    document.getElementById("masajes-faciales").style.display="none";
+    document.getElementById("masajes-reductores").style.display="none";
+    document.getElementById("masajes-moldeadores").style.display="none";
+}
+
+function mostrar1(){
+    document.getElementsByClassName("masajes-relajantes").style.display="block";
+}
+
+function ocultar1(){
+    document.getElementById("masajes-relajantes").style.display="none";
+}
+
+function mostrar2(){
+    document.getElementsById("masajes-faciales").style.display="block";
+}
+
+function ocultar2(){
+    document.getElementById("masajes-faciales").style.display="none";
+}
+
+function mostrar3(){
+    document.getElementsByClassName("masajes-moldeadores").style.display="block";
+}
+
+function ocultar3(){
+    document.getElementById("masajes-reductores").style.display="none";
+}
