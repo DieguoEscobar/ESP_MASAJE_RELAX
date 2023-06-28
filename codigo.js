237cd433057842b227a95ef561f4e7e2cbec0dcd
Dieguo <dieguoescobar@gmail.com>
2023-06-28 18:56:51 -0500

    Ultima versión

--- codigo.js
+++ codigo.js
@@ -3,61 +3,66 @@ function ocultar(){
     document.getElementById("masajes-relajantes").style.display="none";
     document.getElementById("masajes-faciales").style.display="none";
     document.getElementById("masajes-moldeadores").style.display="none";
-    document.getElementById("FlechaReductorUp").style.display="none";
-    document.getElementById("FlechaRelajanteUp").style.display="none";
-    document.getElementById("FlechaFacialUp").style.display="none";
-    document.getElementById("FlechaMoldeadorUp").style.display="none";
+    document.getElementById("modReductores1").style.display="none";
+    document.getElementById("modRelajantes1").style.display="none";
+    document.getElementById("modFaciales1").style.display="none";
+    document.getElementById("modMoldeadores1").style.display="none";
 }
 
 function mostrar1(){
     document.getElementById("masajes-reductores").style.display="block";
-    document.getElementById("FlechaReductor").style.display="none";
-    document.getElementById("FlechaReductorUp").style.display="block";
+    document.getElementById("modReductores").style.display="none";
+    document.getElementById("modReductores1").style.display="block";
 
 }
 
 function mostrar2(){
     document.getElementById("masajes-relajantes").style.display="block";
-    document.getElementById("FlechaRelajante").style.display="none";
-    document.getElementById("FlechaRelajanteUp").style.display="block";
+    document.getElementById("modRelajantes").style.display="none";
+    document.getElementById("modRelajantes1").style.display="block";
+
 
 }
 
 function mostrar3(){
     document.getElementById("masajes-faciales").style.display="block";
-    document.getElementById("FlechaFacial").style.display="none";
-    document.getElementById("FlechaFacialUp").style.display="block";
+    document.getElementById("modFaciales").style.display="none";
+    document.getElementById("modFaciales1").style.display="block";
+
 }
 
 function mostrar4(){
     document.getElementById("masajes-moldeadores").style.display="block";
-    document.getElementById("FlechaMoldeador").style.display="none";
-    document.getElementById("FlechaMoldeadorUp").style.display="block";
+    document.getElementById("modMoldeadores").style.display="none";
+    document.getElementById("modMoldeadores1").style.display="block";
+
 }
 
 
 function ocultar1(){
     document.getElementById("masajes-reductores").style.display="none";
-    document.getElementById("FlechaReductor").style.display="block";
-    document.getElementById("FlechaReductorUp").style.display="none";
+    document.getElementById("modReductores").style.display="block";
+    document.getElementById("modReductores1").style.display="none";
 }
 
 function ocultar2(){
     document.getElementById("masajes-relajantes").style.display="none";
-    document.getElementById("FlechaRelajante").style.display="block";
-    document.getElementById("FlechaRelajanteUp").style.display="none";
+    document.getElementById("modRelajantes").style.display="block";
+    document.getElementById("modRelajantes1").style.display="none";
 }
 
 function ocultar3(){
     document.getElementById("masajes-faciales").style.display="none";
-    document.getElementById("FlechaFacial").style.display="block";
-    document.getElementById("FlechaFacialUp").style.display="none";
+    document.getElementById("modFaciales").style.display="block";
+    document.getElementById("modFaciales1").style.display="none";
+
 }
 
 function ocultar4(){
     document.getElementById("masajes-moldeadores").style.display="none";
-    document.getElementById("FlechaMoldeador").style.display="block";
-    document.getElementById("FlechaMoldeadorUp").style.display="none";
+    document.getElementById("modMoldeadores").style.display="block";
+    document.getElementById("modMoldeadores1").style.display="none";
+    
 }
 
 var images = document.querySelectorAll('#image-container img');
@@ -71,3 +76,13 @@ setInterval(function() {
     }
     images[currentImageIndex].style.opacity = '1';
 }, 3000);
+
+
+function Historia(){
+    window.location.href = 'Nosotros.html';
+}
+
+function Inicio(){
+    window.location.href = 'Index.html';
+    
+}
